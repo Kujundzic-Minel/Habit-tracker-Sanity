b33cd1d76961db935fc50f893cdb7503b10f802e
MINEL <minel.kujundzic@edu.univ-fcomte.fr>
2024-12-09 13:35:03 +0100

    feature(Sanity) : Ajout header

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -2,5 +2,6 @@ import {postType} from './postType'
 import {categoryType} from './categoryType'
 import {seo} from './blocks/seo'
 import { homepage } from './homepage'
+import { header } from './header'
 
-export const schemaTypes = [postType, categoryType, seo, homepage]
+export const schemaTypes = [postType, categoryType, seo, homepage, header]
